77723723f9a1c71df9c77c23310e1e4f5ee4a1b6
Andrew C <ajcolella@gmail.com>
2015-07-17 19:42:05 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# Gemfile
+
+source 'https://rubygems.org'
+ruby "2.2.0"
+
+gem "sinatra"
+
+group :development do
+ gem 'sqlite3'
+ gem "tux"
+end
+
+group :production do
+ gem 'pg'
+end
---
+++ app.rb
@@ -0,0 +1,37 @@
+# require app dependencies
+require 'rubygems'
+require 'sinatra'
+require 'twilio-ruby'
+require './environments'
+
+
+# create a route to render the home page
+get '/' do
+  erb :index
+end
+ 
+# create a route to handle the POST request to the form
+post '/send' do
+
+  client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], 
+    ENV['TWILIO_AUTH_TOKEN']
+ 
+  # Get POST parameters submitted by the user through the form
+  to_number = params[:phone]
+  media_url = params[:url]
+  message_text = params[:message]
+ 
+  # Send a message!
+  message = client.account.messages.create(
+    :to => to_number,
+    :body => message_text,
+    :media_url => media_url,
+ 
+    # This is the MMS enabled number you purchased previously - replace
+    # the environment variable with a string like "+16518675309"
+    :from => ENV['TWILIO_NUMBER']
+  )
+ 
+  # Send back a simple text response that the message was sent
+  "Message sent! SID: #{message.sid}"
+end
