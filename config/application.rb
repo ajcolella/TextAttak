07bfc4067256d9753f35397ff14e12dda47ac555
Andrew C <ajcolella@gmail.com>
2015-07-21 18:29:56 -0400

    cors

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,12 @@
 module App
   class Application < Rails::Application
     config.middleware.insert_before 0, "Rack::Cors" do
+      puts '********************************************************'
       allow do
         origins '*'
         resource '*',
         :headers => :any,
-        :expose => ['X-User-Authentication-Token', 'X-User-Id'],
+        :expose => :any,
         :methods => [:get, :post, :options, :patch, :delete]
       end
     end
